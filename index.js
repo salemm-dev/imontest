a2f75c66e8555b3c643f571aef76a233f57ff34d
salemm-dev <emmanueljuarez@drata.com>
2023-03-22 09:52:17 -0600

    test my gpg test signature

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 window.onload(ev, ()=> {
     console.log("We are on test")
 })
+
+class Kain {
+    constructor(){
+        this.name = 'Kain'
+    }
+
+    sayHello() {
+        console.log('whats up bro')
+    }
+}
+
+const kain = new Kain()
+
+module.exports =  kain;
